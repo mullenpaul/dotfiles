046d78e01bb4369b9a5bd53805a7eaf439a27359
Paul <mullenpaul90@gmail.com>
2024-11-04 00:55:35 +0000

    adding basic neovim configs

---
+++ .config/nvim/init.lua
@@ -0,0 +1,55 @@
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("config.lazy")
+
+require("mason").setup()
+
+
+require("catppuccin").setup({
+	flavour = "mocha",
+	dim_inactive={
+		enabled=false,
+		shade="dark",
+		percentage=0.5,
+	},
+})
+
+vim.g.mapleader = ","
+
+require("nvim-tree").setup({})
+
+require("telescope").setup({
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+  extensions = {
+    -- Your extension configuration goes here:
+    -- extension_name = {
+    --   extension_config_key = value,
+    -- }
+    -- please take a look at the readme of the extension you want to configure
+	}
+})
+
+require ("config.telescope")
+vim.cmd.colorscheme "catppuccin"
